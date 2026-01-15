c047ef059de0134382498428a9257f366c902572
vanphuTteam <vanphu120107@gmail.com>
2026-01-15 23:34:24 +0700

    search

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,15 +2,23 @@ import { Request, Response } from "express";
 
 import Task from "../module/task.module";
 import paginationHelper from "../../../helpers/pagination";
+import searchHelper from "../../../helpers/search";
+import { title } from "node:process";
 
 export const index = async (req: Request, res: Response) => {
     // Find
-    const find = {
+    interface find {
+        deleted: boolean,
+        status?: string,
+        title?: RegExp
+    }
+
+    const find: find = {
         deleted: false
     };
 
     if (req.query.status) {
-        find["status"] = req.query.status;
+        find.status = req.query.status.toString();
     }
     // End Find
 
@@ -22,26 +30,33 @@ export const index = async (req: Request, res: Response) => {
         sort[sortKey] = req.query.sortValue
     }
     // End sort
-    
-     // Pagination
-        let initPagination = {
-            currentPage: 1,
-            limitItems: 2
-        };
-        const countTask = await Task.countDocuments(find);
-        const objectPagination = paginationHelper(
-            initPagination,
-            req.query,
-            countTask
-        );
-        // End Pagination
-        
+
+    // Pagination
+    let initPagination = {
+        currentPage: 1,
+        limitItems: 2
+    };
+    const countTask = await Task.countDocuments(find);
+    const objectPagination = paginationHelper(
+        initPagination,
+        req.query,
+        countTask
+    );
+    // End Pagination
+
+    // Search
+    let objectSearch = searchHelper(req.query);
+    if (req.query.keyword) {
+        find.title= objectSearch.regex;
+    }
+    // End Search
+
     const tasks = await Task
         .find(find)
         .sort(sort)
         .limit(objectPagination.limitItems)
         .skip(objectPagination.skip);
-        
+
     res.json({
         tasks: tasks
     })
