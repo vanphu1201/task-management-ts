63d74b374143aeaa4303727ce7af5e6425c5dd48
vanphuTteam <vanphu120107@gmail.com>
2026-01-17 16:36:17 +0700

    cài đặt cors

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { type Express, type Request, type Response } from "express";
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import * as database from "./config/database";
 import mainV1Route from "./api/v1/routes/index.route";
@@ -11,6 +12,13 @@ database.connect();
 const app: Express = express();
 const port: string | number = process.env.PORT;
 
+// const corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(bodyParser.json());
 
 mainV1Route(app);
